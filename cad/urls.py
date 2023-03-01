c4d9c9ac6a3ad545e08884bc10a64c8007da17c3
Anna Kholkina <kholkina.a2021@gmail.com>
2023-03-01 19:03:30 +0300

    Интерполяция строк

--- cad/urls.py
+++ cad/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('add_unit', views.add_unit, name='add_unit'),
     #path('change_status_10_6', views.change_status_10_6, name='change_status_10_6'),
 
-    path(r'^change_status/(?P<username>\d+)/(?P<status>\d+)$', views.change_status, name='change_status'),
+    path('change_status/<str:username>/<str:status>', views.change_status, name='change_status'),
     path('add_member', views.add_member, name='add_member')
 ]
